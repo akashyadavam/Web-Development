698f0119626453375ad0d0ebc1e101f428104ad9
akashyadavam <akashyadavam585ak@gmail.com>
2026-08-17 08:14:35 +0530

    add

---
+++ JavaScript/Part1/script.js
@@ -0,0 +1,31 @@
+console.log(square);
+var square = (num) => num * num;
+console.log(square);
+
+var n = 4;
+
+var result = square(n);
+
+
+
+
+
+
+//hosting
+
+//getName2();// var getname is variable first time get undefined we cant call undefined we get type eror//give type error
+// getName();
+console.log(x);
+console.log( getName);
+var x=7;
+
+var getName = (aa) => {//arrow function 
+    console.log("Namaste JavaScript");
+};
+console.log( getName);
+getName();//now work first name get undefined second time get whole fn code 
+var getName2 = function () {
+    console.log("Namaste JavaScript");
+};
+console.log( getName2);
+getName2();// var getname is variable first time get undefined we cant call undefined we get type eror
